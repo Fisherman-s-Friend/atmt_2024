9b0d3e74bf93359f2f884f3298c7e5ebade78bec
popos <saphirblau@gmx.ch>
2024-11-05 12:42:09 +0100

    added log files

--- assignments/03/detokenizer.py
+++ assignments/03/detokenizer.py
@@ -1,8 +1,8 @@
 from tokenizers import Tokenizer
 
 tokenizer = Tokenizer.from_file("/home/popos/PycharmProjects/atmt_2024_shared/data/en-fr/bpe_new/tokenizer.json")
-path = "/home/popos/PycharmProjects/atmt_2024_shared/assignments/03/modelbpe128_new/translations.txt"
-with open(path, "r") as file, open("/home/popos/PycharmProjects/atmt_2024_shared/assignments/03/modelbpe128_new/detokenized.txt", "w") as out:
+path = "/assignments/03/models_not_in_report/modelbpe128_new/translations.txt"
+with open(path, "r") as file, open("/assignments/03/models_not_in_report/modelbpe128_new/detokenized.txt", "w") as out:
         for line in file:
             ids = [int(i) for i in line.split()]
             out.write(tokenizer.decode(ids))
