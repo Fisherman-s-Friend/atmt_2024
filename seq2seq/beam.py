0947352e08584cd5b40b61d7dce3749d5938e25a
Fisherman-s-Friend <dominicphilipp.fischer@uzh.ch>
2024-12-09 22:54:18 +0100

    minor aesthetic changes

--- seq2seq/beam.py
+++ seq2seq/beam.py
@@ -5,31 +5,34 @@ from queue import PriorityQueue
 
 
 class BeamSearch(object):
-    """ Defines a beam search object for a single input sentence. """
+    """Defines a beam search object for a single input sentence."""
+
     def __init__(self, beam_size, max_len, pad):
 
         self.beam_size = beam_size
         self.max_len = max_len
         self.pad = pad
 
-        self.nodes = PriorityQueue() # beams to be expanded
-        self.final = PriorityQueue() # beams that ended in EOS
+        self.nodes = PriorityQueue()  # beams to be expanded
+        self.final = PriorityQueue()  # beams that ended in EOS
 
-        self._counter = count() # for correct ordering of nodes with same score
+        self._counter = count()  # for correct ordering of nodes with same score
 
     def add(self, score, node):
-        """ Adds a new beam search node to the queue of current nodes """
+        """Adds a new beam search node to the queue of current nodes"""
         self.nodes.put((score, next(self._counter), node))
 
     def add_final(self, score, node):
-        """ Adds a beam search path that ended in EOS (= finished sentence) """
+        """Adds a beam search path that ended in EOS (= finished sentence)"""
         # ensure all node paths have the same length for batch ops
         missing = self.max_len - node.length
-        node.sequence = torch.cat((node.sequence.cpu(), torch.tensor([self.pad]*missing).long()))
+        node.sequence = torch.cat(
+            (node.sequence.cpu(), torch.tensor([self.pad] * missing).long())
+        )
         self.final.put((score, next(self._counter), node))
 
     def get_current_beams(self):
-        """ Returns beam_size current nodes with the lowest negative log probability """
+        """Returns beam_size current nodes with the lowest negative log probability"""
         nodes = []
         while not self.nodes.empty() and len(nodes) < self.beam_size:
             node = self.nodes.get()
@@ -37,7 +40,7 @@ class BeamSearch(object):
         return nodes
 
     def get_best(self):
-        """ Returns final node with the lowest negative log probability """
+        """Returns final node with the lowest negative log probability"""
         # Merge EOS paths and those that were stopped by
         # max sequence length (still in nodes)
         merged = PriorityQueue()
@@ -54,21 +57,23 @@ class BeamSearch(object):
 
         return node
 
-
     def prune(self):
-        """ Removes all nodes but the ones having a better probability that the current best finished one """
+        """Removes all nodes but the ones having a better probability that the current best finished one"""
 
         nodes = PriorityQueue()
         finished = self.final.qsize()
-        best_final_score = float('inf') if finished == 0 else self.final.queue[0][0] # get logpob score of the best finished sentence
+        best_final_score = (
+            float("inf") if finished == 0 else self.final.queue[0][0]
+        )  # get logpob score of the best finished sentence
         while not self.nodes.empty():
             node = self.nodes.get()
             if node[0] < best_final_score:
                 nodes.put(node)
             if nodes.qsize() >= self.beam_size:
-                    break
+                break
 
         self.nodes = nodes
+
     '''
 
 
@@ -77,12 +82,8 @@ class BeamSearch(object):
         nodes = PriorityQueue()
         for _ in range(self.nodes.qsize()):
             node = self.nodes.get()
-            if node[2].length >= self.max_len:
-                break
-                #self.add_final(node[0], node[2])
-            else:
-                if nodes.qsize() < self.beam_size: # make sure not to exceed beam size
-                    nodes.put(node)
+            if node[2].length < self.max_length and nodes.qsize() < self.beam_size: # make sure not to exceed beam size
+                nodes.put(node)
         self.nodes = nodes
 
 
@@ -100,9 +101,23 @@ class BeamSearch(object):
         self.nodes = nodes
     
     '''
+
+
 class BeamSearchNode(object):
-    """ Defines a search node and stores values important for computation of beam search path"""
-    def __init__(self, search, emb, lstm_out, final_hidden, final_cell, mask, sequence, logProb, length):
+    """Defines a search node and stores values important for computation of beam search path"""
+
+    def __init__(
+        self,
+        search,
+        emb,
+        lstm_out,
+        final_hidden,
+        final_cell,
+        mask,
+        sequence,
+        logProb,
+        length,
+    ):
 
         # Attributes needed for computation of decoder states
         self.sequence = sequence
@@ -119,15 +134,14 @@ class BeamSearchNode(object):
         self.search = search
 
     def eval(self, alpha=0.0):
-        """ Returns score of sequence up to this node 
+        """Returns score of sequence up to this node
 
-        params: 
+        params:
             :alpha float (default=0.0): hyperparameter for
             length normalization described in in
             https://arxiv.org/pdf/1609.08144.pdf (equation
             14 as lp), default setting of 0.0 has no effect
-        
+
         """
-        normalizer = (5 + self.length)**alpha / (5 + 1)**alpha
+        normalizer = (5 + self.length) ** alpha / (5 + 1) ** alpha
         return self.logp / normalizer
-        
